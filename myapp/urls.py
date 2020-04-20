375f9aaadcf567a1c588f00b9e6b57398331f4e5
Munazza Fatma <fatmamunazza@gmail.com>
2020-04-20 12:38:20 +0530

    DetaiLPage backend completed

--- myapp/urls.py
+++ myapp/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('', views.index),
     path('tripMateForm/', views.tripmateform, name="tripMateForm"),
     path('check', views.check, name="check"),
-    path('selectedList/<str:id>/<int:id1>', views.selectedList, name="selectedList/<str:id>/<int:id1>"),
-    path('selectedList/selected/detailsPage', views.detailsPage, name="detailsPage"),
+    path('selectedList/<str:id>/<int:id1>', views.selectedList, name="selectedList"),
+    path('selectedList/<str:list>/detailsPage/<int:value>', views.detailsPage, name="detailsPage"),
 ]
